8145cb041ba0d27a1ccc10d1ab97511f95af2727
Shakirul Hasan <theshk.me@gmail.com>
2019-02-06 03:37:46 +0600

    CF 1093A

---
+++ Codeforces/1093 - A - Dice Rolling.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        int x;
+        cin>>x;
+        cout<<x/6 + 1<<endl;
+    }
+}
