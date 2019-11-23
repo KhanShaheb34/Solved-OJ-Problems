4cd6798560b1b3de678cb72c16e56f5367bf0933
Shakirul Hasan <shakirul34@student.sust.edu>
2019-11-23 20:20:51 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,48 @@
+
+/*
+* @site         
+* @problem_no   
+* @problem_name 
+* @author       KhanShaheb
+* @Language     C++
+* @status       
+* @time         
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+inline void fast_io() { ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define INF 1000000000
+#define endl '\n'
+#define ashche cerr<<"Ekhane Ashche!"<<endl
+#define mem(a,x) memset(a,x,sizeof(a))
+#define fout(x) fixed<<setprecision(x)
+const int mod=1e9+7;
+void deb(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void deb(istream_iterator<string> it, T a, Args... args) {
+    cerr << *it << " = " << a << endl;
+    deb(++it, args...);
+}
+
+int main()
+{
+    fast_io();
+
+    int q;      // FOR TESTCASES
+    cin >> q;
+    while(q--)
+    {
+        // CODE HERE
+        
+    }
+
+    return 0;
+}
