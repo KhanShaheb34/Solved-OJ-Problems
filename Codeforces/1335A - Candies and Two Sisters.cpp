d740ac48e467399c748e37729275336818f145f8
Shakirul Hasan <shakirul34@student.sust.edu>
2020-04-14 00:12:57 +0600

    Create 1335A - Candies and Two Sisters.cpp

---
+++ Codeforces/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(ll cs) {
+    ll n;
+    cin >> n;
+    cout << ((n+1)/2) - 1 << endl;
+}
+
+int main() {
+    ll t;
+    cin >> t;
+    for(ll i = 0; i < t; i++) {
+        solve(i+1);
+    }
+    return 0;
+}
