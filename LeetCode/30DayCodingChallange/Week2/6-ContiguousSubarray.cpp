b1acc83a76ee6772aed441580edc873db63cfee5
Shakirul Hasan <shakirul34@student.sust.edu>
2020-04-13 15:48:38 +0600

    Create 6-ContiguousSubarray.cpp

---
+++ LeetCode/30DayCodingChallange/Week2/6-ContiguousSubarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+       
+        map<int, vector<int> > rep;
+        int maxlen = 0, n = nums.size(), cp = 0;
+        rep[0].push_back(-1);
+        for(int i=0; i<n; i++) {
+            if(nums[i]) cp++;
+            else cp--;
+            rep[cp].push_back(i);
+            maxlen = max(maxlen, i-rep[cp][0]);
+        }
+        return maxlen;
+    }
+};
