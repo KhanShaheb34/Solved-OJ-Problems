85c0a00f2a0a42d0ae08d3be09e5d6c4f48f80f3
Shakirul Hasan <shakirul34@student.sust.edu>
2020-04-16 16:34:39 +0600

    Create 1337A - Ichihime and Triangle.cpp

---
+++ Codeforces/1337A - Ichihime and Triangle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(ll cs) {
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << endl;
+}
+ 
+int main() {
+    ll t;
+    cin >> t;
+    for(ll i = 0; i < t; i++) {
+        solve(i+1);
+    }
+    return 0;
+}
