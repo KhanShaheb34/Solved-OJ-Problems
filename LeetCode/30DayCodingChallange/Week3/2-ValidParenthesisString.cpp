9844c55f78dbf92df415c5e996727c67d72ff986
Shakirul Hasan <shakirul34@student.sust.edu>
2020-04-20 01:50:03 +0600

    Create 2-ValidParenthesisString.cpp

---
+++ LeetCode/30DayCodingChallange/Week3/2-ValidParenthesisString.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.length();
+        int balance = 0;
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(' || s[i] == '*')
+                balance++;
+            else {
+                balance--;
+                if(balance < 0)
+                    return false;
+            }
+        }
+        balance = 0;
+        for(int i=n-1; i>=0; i--) {
+            if(s[i] == ')' || s[i] == '*')
+                balance++;
+            else {
+                balance--;
+                if(balance < 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
