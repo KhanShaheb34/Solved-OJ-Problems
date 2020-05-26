a2eb8660265c3c94947b0584745705046dd7ad76
Shakirul Hasan <shakirul34@student.sust.edu>
2020-05-27 00:16:50 +0600

    Create 1358A - Park Lighting.cpp

---
+++ Codeforces/1358A - Park Lighting.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve(ll cs) {
+  ll n, m;
+  cin >> n >> m;
+  cout << (n * m + 1) / 2 << endl;
+}
+
+int main() {
+  FastIO; ll t; cin >> t; for (ll i = 0; i < t; i++) solve(i + 1);
+  return 0;
+}
