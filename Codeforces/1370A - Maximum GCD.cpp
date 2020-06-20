690465e8885106875a232d900ecdedb8f13018ae
Shakirul Hasan <shakirul34@student.sust.edu>
2020-06-21 00:30:40 +0600

    Create 1370A - Maximum GCD.cpp

---
+++ Codeforces/1370A - Maximum GCD.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void solve(ll cs) {
+  int n;
+  cin >> n;
+  cout << n / 2 << endl;
+}
+
+int main() {
+  FastIO; ll t; cin >> t; for (ll i = 0; i < t; i++) solve(i + 1); return 0;
+}
